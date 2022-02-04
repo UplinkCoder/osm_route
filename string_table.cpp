1ed73ec2a4f84934021a6d04598ac73e1b8b8328
Stefan Koch <uplink.coder@googlemail.com>
2022-02-04 06:58:16 +0100

    Remove whitespace

--- string_table.cpp
+++ string_table.cpp
@@ -45,7 +45,7 @@ struct StringTable
 
     /// Returns 0 if not found or the index of the string_entry + 1 if found
     uint32_t LookupString(const string_view& str);
-    
+
     /// Returns 0 if not found or the index fo the string_entry + 1 if found
     uint32_t LookupString (const char* str_data, uint32_t str_size, uint32_t input_crc);
 
@@ -128,13 +128,14 @@ uint32_t StringTable::LookupString (const string_view& str) {
 
     const uint32_t crc_input =
         FINALIZE_CRC32C(crc32c(INITIAL_CRC32C, str_data, str_size));
-        
+
     return LookupString(str_data, str_size, crc_input);
 }
 
 uint32_t StringTable::LookupString (const char* str_data, uint32_t str_size, uint32_t crc_input) {
     uint idx = 0;
     // using entry_t = decltype(strings)::value_type;
+
     for (auto it = crc32_to_indecies.find(crc_input);
          it != crc32_to_indecies.end();
          it++
